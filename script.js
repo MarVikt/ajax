07b23a29c03cc3874ca1f97bf6543869bbac4ca6
MarVikt <v.m.podporogie@gmail.com>
2022-05-03 23:47:26 +0300

    completed task#26

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
-const user = 'db.json';
-const dataBaseUrl = 'https://jsonplaceholder.typicode.com/posts';
-let str = '';
+  const user = 'db.json';
+  const dataBaseUrl = 'https://jsonplaceholder.typicode.com/posts';
+  
+  const getData = (file) => {
+    return fetch(file)
+      .then(response => response.json());
+  };
+  
+  const sendData = (url,data) => {
+    return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+      }).then(response => response.json());
+  };
+  
+  const promise = (userData) => {
+    return new Promise ((resolve,reject) => {
+      resolve(getData(userData));
+    });
+  };
+  
+  promise(user)
+    .then(data => sendData(dataBaseUrl,data))
+    .then(data => console.log(data))
+    .catch((errMessage) => console.log(errMessage))
+    .finally(() => console.log('finally'));
 
-const getData = (file) => {
-  return fetch(file)
-    .then(response => response.json())
-    // .then(data => JSON.stringify(data))
-    .catch(error => console.log(error));
-};
-
-const sendData = (url,data) => {
-  return fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    }
-    }).then(response => response.json());
-};
-// str = getData(dataBaseUrl);
-sendData(dataBaseUrl,getData(user))
-  .then(data => console.log(data));
+  // 
+  // const user = 'db.json';
+  // const dataBaseUrl = 'https://jsonplaceholder.typicode.com/posts';
+  // let str = '';
+  
+  // const getData = (file) => {
+  //   return fetch(file)
+  //     .then(response => response.json())
+  //     .catch(error => console.log(error));
+  // };
+  
+  // const sendData = (url,data) => {
+  //   return fetch(url, {
+  //     method: 'POST',
+  //     body: JSON.stringify(data),
+  //     headers: {
+  //       'Content-type': 'application/json; charset=UTF-8',
+  //     }
+  //     }).then(response => response.json());
+  // };
+  
+  // sendData(dataBaseUrl,getData(user))
+  //   .then(data => console.log(data));
+    
